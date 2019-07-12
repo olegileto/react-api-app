bd4138019adf76f8c5095fc84d43478ab105bc56
Oleg Khmelevskoy <olegileto@gmail.com>
2019-07-12 18:24:10 +0300

    Minor fix

--- src/components/RandomPlanet/RandomPlanet.js
+++ src/components/RandomPlanet/RandomPlanet.js
@@ -29,7 +29,7 @@ export default class RandomPlanet extends Component {
     };
 
     updatePlanet() {
-        const id = Math.floor(Math.random() * 25) + 2;
+        const id = Math.floor(Math.random() * 10) + 2;
 
         this.swapiServices
             .getPlanet(id)
